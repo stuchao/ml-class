eafe7e64b7edf900e0983b1f9b28c4f647406be4
stuchao <stuchao@local>
2018-09-12 14:13:01 +0000

    Debug information on data sets

--- keras-perceptron/perceptron-linear.py
+++ keras-perceptron/perceptron-linear.py
@@ -13,6 +13,16 @@ config = run.config
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
+#print ('====')
+#print (X_train)
+#print (y_train)
+#print (X_train.shape)
+#print (y_train.shape)
+#print (X_test.shape)
+#print (y_test.shape)
+#print ('====')
+#exit()
+
 img_width = X_train.shape[1]
 img_height = X_train.shape[2]
 
